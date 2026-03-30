c12c1e4807842121fa4d12246f69b6729d6cb65c
Sohrab <sohrab@nexaai.dev>
2026-03-30 23:57:13 +0000

    chore: inject 21st SmoothScrollHero into homepage

--- sleek-portfolio/src/pages/index.jsx
+++ sleek-portfolio/src/pages/index.jsx
@@ -2,10 +2,13 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Work from '@/components/Work'
 import Contact from '@/components/Contact'
+import SmoothScrollHero from '@/components/ui/modern-hero/SmoothScrollHero'
 
 export default function Home(){
   return (
     <main className="bg-gray-900 text-slate-100 min-h-screen">
+      {/* 21st modern hero injected here */}
+      <SmoothScrollHero />
       <Hero />
       <About />
       <Work />
